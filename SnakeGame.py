ca482d30852fc91fa073372497452bd8919449a8
David Morgenstern <DavidtheConfident@gmail.com>
2022-10-01 21:06:17 -0500

    Creates cube Class

--- SnakeGame.py
+++ SnakeGame.py
@@ -3,3 +3,18 @@ import random
 import pygame
 import tkinter as tk
 from tkinter import messagebox
+
+class cube(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+    
