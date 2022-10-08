aa855ce862549d1f627bf1aee6a7266f209ba6d6
David Morgenstern <DavidtheConfident@gmail.com>
2022-10-07 21:20:59 -0500

    Makes sure Snake doesn't go past borders

--- SnakeGame.py
+++ SnakeGame.py
@@ -29,7 +29,49 @@ class snake(object):
         self.dirny = 1
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for keys in keys():
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows - 1, c.pos[-1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows - 1: c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows - 1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows - 1)
+                else: c.move(c.dirnx, c.dirny)
+
 
     def reset(self, pos):
         pass
