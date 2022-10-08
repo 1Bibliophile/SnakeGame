3cf642d72a63b602e55599341ddeeb3157a41fe1
David Morgenstern <DavidtheConfident@gmail.com>
2022-10-07 20:33:20 -0500

    Draws Grid

--- SnakeGame.py
+++ SnakeGame.py
@@ -11,7 +11,7 @@ class cube(object):
     def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
         pass
 
-    def move(self, dirnx, dirny):
+    def move(self, dirnx,  dirny):
         pass
 
     def draw(self, surface, eyes=False):
@@ -34,9 +34,23 @@ class snake(object):
         pass
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBtw = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtw
+        y = y + sizeBtw
+
+        pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
 
 def redrawWindow(surface):
+    global rows, width
+    surface.fill((0,0,0))
+    drawGrid(width, row, surface)
+    
+    pygame.display.update()
     pass
 
 def randomSnack(rows, items):
@@ -46,11 +60,12 @@ def messageBox(subject, content):
     pass
 
 def main():
+    global width, rows
     width = 500
-    height = 500
+    #height = 500
     rows = 20
 
-    win = pygame.display.set_mode((width, height))
+    win = pygame.display.set_mode((width, width))
     s = snake((255, 0, 0), (10, 10))
 
     flag = True
@@ -59,7 +74,11 @@ def main():
 
     while flag:
         pygame.time.delay(50)
+
+        # Makes sure the game doesn't more than 10 frames/second
         clock.tick(10)
+
+        redrawWindow(win)
          
     pass
 
@@ -67,7 +86,7 @@ def main():
 # w =
 # h =
 
-cube.rows = rows
-cube.w = w
+#cube.rows = rows
+#cube.w = w
 
 main()
